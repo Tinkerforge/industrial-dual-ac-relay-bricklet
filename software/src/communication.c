efcb2208f04f8ac9222289a575a1be43999bf3d8
Olaf Lüke <olaf@tinkerforge.com>
2020-09-01 10:54:36 +0200

    Add Industrial Dual AC Relay Bricklet firmware implementation

---
+++ software/src/communication.c
@@ -0,0 +1,146 @@
+/* industrial-dual-ac-relay-bricklet
+ * Copyright (C) 2020 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * communication.c: TFP protocol message handling
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "communication.h"
+#include "relay.h"
+
+#include "bricklib2/utility/communication_callback.h"
+#include "bricklib2/hal/system_timer/system_timer.h"
+#include "bricklib2/protocols/tfp/tfp.h"
+
+extern Relay relay;
+
+BootloaderHandleMessageResponse handle_message(const void *message, void *response) {
+	switch(tfp_get_fid_from_message(message)) {
+		case FID_SET_VALUE: return set_value(message);
+		case FID_GET_VALUE: return get_value(message, response);
+		case FID_SET_MONOFLOP: return set_monoflop(message);
+		case FID_GET_MONOFLOP: return get_monoflop(message, response);
+		case FID_SET_SELECTED_VALUE: return set_selected_value(message);
+		default: return HANDLE_MESSAGE_RESPONSE_NOT_SUPPORTED;
+	}
+}
+
+BootloaderHandleMessageResponse set_value(const SetValue *data) {
+	relay_set_value(0, data->channel0);
+	relay_set_value(1, data->channel1);
+
+	relay.monoflop_running[0] = false;
+	relay.monoflop_running[1] = false;
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_value(const GetValue *data, GetValue_Response *response) {
+	response->header.length = sizeof(GetValue_Response);
+	response->channel0      = relay_get_value(0);
+	response->channel1      = relay_get_value(1);
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse set_monoflop(const SetMonoflop *data) {
+	if(data->channel > 1) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	relay.monoflop_start[data->channel]   = system_timer_get_ms();
+	relay.monoflop_time[data->channel]    = data->time;
+	relay.monoflop_running[data->channel] = true;
+	relay_set_value(data->channel, data->value);
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+BootloaderHandleMessageResponse get_monoflop(const GetMonoflop *data, GetMonoflop_Response *response) {
+	if(data->channel > 1) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	response->header.length  = sizeof(GetMonoflop_Response);
+	response->value          = relay_get_value(data->channel);
+	response->time           = relay.monoflop_time[data->channel];
+
+	if(relay.monoflop_running[data->channel]) {
+		response->time_remaining = relay.monoflop_time[data->channel] - (uint32_t)(system_timer_get_ms() - relay.monoflop_start[data->channel]);
+	} else {
+		response->time_remaining = 0;
+	}
+
+	// Sanity check time remaining
+	if(response->time_remaining > response->time) {
+		response->time_remaining = 0;
+	}
+
+	return HANDLE_MESSAGE_RESPONSE_NEW_MESSAGE;
+}
+
+BootloaderHandleMessageResponse set_selected_value(const SetSelectedValue *data) {
+	if(data->channel > 1) {
+		return HANDLE_MESSAGE_RESPONSE_INVALID_PARAMETER;
+	}
+
+	relay_set_value(data->channel, data->value);
+
+	relay.monoflop_running[data->channel] = false;
+
+	return HANDLE_MESSAGE_RESPONSE_EMPTY;
+}
+
+bool handle_monoflop_done_callback(void) {
+	static bool is_buffered = false;
+	static MonoflopDone_Callback cb;
+
+	if(!is_buffered) {
+		if(relay.monoflop_callback[0]) {
+			tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(MonoflopDone_Callback), FID_CALLBACK_MONOFLOP_DONE);
+			cb.channel = 0;
+			cb.value = relay_get_value(0);
+			relay.monoflop_callback[0] = false;
+		} else if(relay.monoflop_callback[1]) {
+			tfp_make_default_header(&cb.header, bootloader_get_uid(), sizeof(MonoflopDone_Callback), FID_CALLBACK_MONOFLOP_DONE);
+			cb.channel = 1;
+			cb.value = relay_get_value(1);
+			relay.monoflop_callback[1] = false;
+		} else {
+			return false;
+		}
+	}
+
+	if(bootloader_spitfp_is_send_possible(&bootloader_status.st)) {
+		bootloader_spitfp_send_ack_and_message(&bootloader_status, (uint8_t*)&cb, sizeof(MonoflopDone_Callback));
+		is_buffered = false;
+
+		return true;
+	} else {
+		is_buffered = true;
+	}
+
+	return false;
+}
+
+void communication_tick(void) {
+	communication_callback_tick();
+}
+
+void communication_init(void) {
+	communication_callback_init();
+}
---
+++ software/src/communication.h
@@ -0,0 +1,112 @@
+/* industrial-dual-ac-relay-bricklet
+ * Copyright (C) 2020 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * communication.h: TFP protocol message handling
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "bricklib2/protocols/tfp/tfp.h"
+#include "bricklib2/bootloader/bootloader.h"
+
+// Default functions
+BootloaderHandleMessageResponse handle_message(const void *data, void *response);
+void communication_tick(void);
+void communication_init(void);
+
+// Constants
+
+
+// Function and callback IDs and structs
+#define FID_SET_VALUE 1
+#define FID_GET_VALUE 2
+#define FID_SET_MONOFLOP 3
+#define FID_GET_MONOFLOP 4
+#define FID_SET_SELECTED_VALUE 6
+
+#define FID_CALLBACK_MONOFLOP_DONE 5
+
+typedef struct {
+	TFPMessageHeader header;
+	bool channel0;
+	bool channel1;
+} __attribute__((__packed__)) SetValue;
+
+typedef struct {
+	TFPMessageHeader header;
+} __attribute__((__packed__)) GetValue;
+
+typedef struct {
+	TFPMessageHeader header;
+	bool channel0;
+	bool channel1;
+} __attribute__((__packed__)) GetValue_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t channel;
+	bool value;
+	uint32_t time;
+} __attribute__((__packed__)) SetMonoflop;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t channel;
+} __attribute__((__packed__)) GetMonoflop;
+
+typedef struct {
+	TFPMessageHeader header;
+	bool value;
+	uint32_t time;
+	uint32_t time_remaining;
+} __attribute__((__packed__)) GetMonoflop_Response;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t channel;
+	bool value;
+} __attribute__((__packed__)) MonoflopDone_Callback;
+
+typedef struct {
+	TFPMessageHeader header;
+	uint8_t channel;
+	bool value;
+} __attribute__((__packed__)) SetSelectedValue;
+
+
+// Function prototypes
+BootloaderHandleMessageResponse set_value(const SetValue *data);
+BootloaderHandleMessageResponse get_value(const GetValue *data, GetValue_Response *response);
+BootloaderHandleMessageResponse set_monoflop(const SetMonoflop *data);
+BootloaderHandleMessageResponse get_monoflop(const GetMonoflop *data, GetMonoflop_Response *response);
+BootloaderHandleMessageResponse set_selected_value(const SetSelectedValue *data);
+
+// Callbacks
+bool handle_monoflop_done_callback(void);
+
+#define COMMUNICATION_CALLBACK_TICK_WAIT_MS 1
+#define COMMUNICATION_CALLBACK_HANDLER_NUM 1
+#define COMMUNICATION_CALLBACK_LIST_INIT \
+	handle_monoflop_done_callback, \
+
+
+#endif
